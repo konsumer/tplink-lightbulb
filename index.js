41212b956b5afb240a0dfcb893ebe4f553721a54
David Konsumer <konsumer@jetboystudio.com>
2017-01-10 19:38:26 -0800

    build stuff for users

--- index.js
+++
@@ -1,4 +0,0 @@
-// babel wrapper for lib
-
-require('babel-register')
-module.exports = require('./lib').default
--- tplight.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-// babel-wrapper for cli
-require('babel-register')
-require('./cli')
